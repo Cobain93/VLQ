d25979c22d595f0ff4290f9a4c2b9d0547a6da1b
叶勇斌 <cobain.ye@gmail.com>
2019-05-29 11:26:12 +0800

    构建脚本升级

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,77 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MyPackage = require('./package.json');
 
-module.exports = (function () {
-  const env = process.env;
-  const production = env.NODE_ENV === 'production';
-  const { amd, umd, cjs, old, NODE_ENV: mode } = env;
-  const devtool = production ? '' : 'source-map';
+module.exports = env => {
+  env = env ? env : {}; //环境变量
+  const mode = env.production ? "production" : "development"; //开发或生产模式
+  const devtool = env.production || env.nodevtool ? "" : "source-map"; //
   const entry = {};
-  const libraryTarget = amd ? 'amd' : umd ? 'umd' : cjs ? 'commonjs' : old ? 'umd' : 'commonjs';
-  const libraryTargetPath = amd ? 'amd' : umd ? 'umd' : cjs ? 'cjs' : old ? '' : 'cjs';
+  const plugins = [];
+  const optimization = {};  //优化选项
+  const minimizer = []; //优化选项：瘦身器
+  const libraryTarget = env.amd ? 'amd' : env.umd ? 'umd' : env.cjs ? 'commonjs' : env.old ? 'umd' : 'commonjs';
+  const libraryTargetPath = env.amd ? 'amd' : env.umd ? 'umd' : env.cjs ? 'cjs' : env.old ? '' : 'cjs';
   const distDir = path.resolve(__dirname, 'dist', libraryTargetPath);
-  const plugins = [
-    new HtmlWebpackPlugin({
-      template: './sdktest/index.html',
-      inject: false,
-    })
-  ];
-  if (production) {
+  const mainEntryName = env.production ? "index.min" : "index.dev";
+  entry[mainEntryName] = './src/index.ts';
+  optimization['minimizer'] = minimizer;
+  if (!env.amd) { //非amd(即umd或cjs）模式
     plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        ie8: true,  //瘦身后，支持IE8
-        beautify: false,
-        mangle: {
-          keep_fnames: true
-        },
-        compress: {
-          warnings: false,
-          drop_debugger: true,
-          drop_console: true
-        },
-        comments: false
+      new HtmlWebpackPlugin({
+        template: './sdktest/index.html',
+        inject: false,
       })
     )
   }
-  const mainEntryName = production ? "index.min" : "index.dev";
-  entry[mainEntryName] = './src/index.ts';
+  if (env.production) { //生产模式
+    minimizer.push(
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          ie8: true,  //瘦身后，支持IE8
+          beautify: false,
+          mangle: {
+            keep_fnames: true
+          },
+          comments: false
+        }
+      })
+    )
+  }
+
+
   return {
-    mode,
-    entry,
+    mode: mode,
+    entry: entry,
+    devtool: devtool,
     output: {
       path: distDir,
-      filename: '[name].js',
-      libraryTarget
+      libraryTarget: libraryTarget,
+      filename: "[name].js"
     },
-    devtool,
     resolve: {
-      extensions: ['', '.ts']
+      extensions: [".ts", ".js"]
     },
     module: {
-      loaders: [
+      rules: [
         {
           test: /\.ts$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-          include: __dirname
+          loader: "ts-loader",
+          exclude: /node_modules/
         },
         {
           test: /\.ts$/,
-          loader: `string-replace-loader`,
-          query: {
+          loader: 'string-replace-loader',
+          options: {
             search: '@@VERSION',
             replace: MyPackage.version,
             flags: 'g'
-          },
-          exclude: /node_modules/
+          }
         }
       ]
     },
-    plugins,
-    devServer: production ? false : {
-      port: 3000,
-      host: '0.0.0.0',
-      disableHostCheck: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }
+    plugins: plugins,
+    optimization: optimization
   }
-})();
+}
